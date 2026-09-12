0c08948eed8df3812b87928eb530e85d0161f3a3
mtaas-v0 <zliu.dayjob@gmail.com>
2026-09-12 03:35:05 -0400

    Update args.rs
    [no ci]

--- docs/src/args.rs
+++ docs/src/args.rs
@@ -85,7 +85,7 @@ pub struct CompileArgs {
     pub open: bool,
     
     #[arg(short, long, value_parser = parse_key_val)]
-    pub inputField: Vec<(String, String)>,
+    pub input_fields: Vec<(String, String)>,
 }
 
 /// Helper function to split "key=value" strings
